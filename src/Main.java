4c11c1bcc22cf2b152213e78939f5541dddd80e1
Ayça Aydemir <40012179+codifybalance@users.noreply.github.com>
2021-05-09 02:50:01 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,12 @@ public class Main {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
-	//	Customer customer= new Customer(1,"Ayca","Aydemir",(1993,11,29),"20903286488");
+	//	Customer customer= new Customer(1,"Ayca","Aydemir",(1993,11,29),"20903286400");
 	//	Customer customer= new Customer();
     // customer.FirstName="Ayça";
 	//	Date d1= new Date(1993);	
 		
-		Customer customer1= new Customer(1993,"Ayca","Aydemir","20903286488");
+		Customer customer1= new Customer(1993,"Ayca","Aydemir","20903286400");
 		customerManager.Save(customer1);
 	}
 
